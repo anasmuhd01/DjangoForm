eadd88e4752f1214964b7644e84a405fea2e6c16
anas <ansmuhd01@gmail.com>
2023-10-20 06:56:53 +0530

    list_display

--- job_application/admin.py
+++ job_application/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Form
 # Register your models here.
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+
+
+admin.site.register(Form, FormAdmin)
