cbcfeb12aa82965d725510b7c79a81c0a95a22e0
anas <ansmuhd01@gmail.com>
2023-10-20 07:01:10 +0530

    ordering

--- job_application/admin.py
+++ job_application/admin.py
@@ -7,7 +7,7 @@ class FormAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "email")
     search_fields = ("first_name", "last_name", "email")
     list_filter = ("date", "occupation")
-
+    ordering = ("first_name", )
 
 
 admin.site.register(Form, FormAdmin)
